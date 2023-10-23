0cfea6fc2f4c85e9871b2a702754a84e5a17f1ec
Laviel <fafansin@gmail.com>
2023-10-23 12:46:06 +0300

    Basic CLI example with arguments

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-const fs = require('fs').promises;
-
-fs.readFile("./files/new-task.txt", 'utf-8')
-    .then((content) => {
-        console.log('CONTENT', content);
-        fs.appendFile("./files/tasks.txt", `\n${content}`, "utf-8")
-            .then(() => {
-                console.log("Text added successfully!");
-            })
-            .catch(err => {
-                console.log("ERROR Append:", err);
-            })
-    })
-    .catch(e => {
-        console.log("ERROR", e.message);
-    })
+// CLI EXAMPLE
+const args = process.argv.slice(2);
+const sum = args.reduce((total, num) => total + parseFloat(num), 0);
+console.log('SUM:', sum);
