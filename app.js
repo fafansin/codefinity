fa349940323f1cabd0c4132bc8b1aac1e95feea0
Laviel <fafansin@gmail.com>
2023-10-23 14:11:27 +0300

    CLI Guessing Game

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 // CLI EXAMPLE using readline
+const fs = require('fs').promises;
 const readline = require("node:readline");
 const {stdin: input, stdout:output} = require ('node:process');
-
 const rl = readline.createInterface({input, output});
 
-const fortunes = [
-    "You will find unexpedted joy in the little things",
-    "A new opportunity will open doors for you.",
-    "Adventure is just around the corner.",
-    "Embrace change, and good things will follow",
-    "Patience will bring you great rewards."
-];
+const minNumber = 1;
+const maxNumber = 100;
 
-rl.question("Welcome to the Fortune Teller! What's your name? ", (name) => {
-    const randomIndex = Math.floor(Math.random() * fortunes.length);
-    const randomFortune = fortunes[randomIndex];
+const secretNumber = Math.floor(Math.random() * (maxNumber-minNumber + 1)) + minNumber;
 
-    console.log(`Hello, ${name}! Your fortune today: ${randomFortune}`);
+let attempts = 0;
 
-    rl.close();
-})
+function isValidGuess(guess){
+    return !isNaN(guess)
+        && guess >= minNumber
+        && guess <= maxNumber;
+}
+
+function playGame(){
+    rl.question(`Guess a number between ${minNumber} and ${maxNumber} `, (guess) => {
+        if(isValidGuess(guess)){
+            attempts++;
+            const guessNumber = parseInt(guess);
+            
+            if(guessNumber === secretNumber){
+                console.log(`Congratulations! You guessed the number in ${attempts} attempts!`);
+                saveGameResult(`Player won in ${attempts} attempts. `)
+                rl.close
+            }else if(guessNumber < secretNumber){
+                console.log('Try Higher');
+                playGame();
+            }else{
+                console.log('Try Lower');
+                playGame();
+            }
+        }else{
+            console.log('Please enter a valid number with the specified range')
+        }
+    })
+}
+
+async function saveGameResult(result){
+    try{
+        await fs.appendFile('./files/game_results.txt', `${result}\n`);
+    }catch (err){
+        console.log('Failed to save game result.');
+    }
+}
+
+playGame();
