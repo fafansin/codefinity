68c1c599149c98218a33e94b66224542cbbabc03
Laviel <fafansin@gmail.com>
2023-10-23 11:35:42 +0300

    FS Module read and write exercise

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-// Multiplier
-const ordererQuantity = 4;
-const pricePerTumbler = 49;
-const message = "You have to pay:";
-console.log(message, ordererQuantity*pricePerTumbler);
+const fs = require('fs').promises;
+
+fs.readFile("./files/new-task.txt", 'utf-8')
+    .then((content) => {
+        console.log('CONTENT', content);
+        fs.appendFile("./files/tasks.txt", `\n${content}`, "utf-8")
+            .then(() => {
+                console.log("Text added successfully!");
+            })
+            .catch(err => {
+                console.log("ERROR Append:", err);
+            })
+    })
+    .catch(e => {
+        console.log("ERROR", e.message);
+    })
